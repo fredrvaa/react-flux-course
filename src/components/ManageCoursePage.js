76c6c160d4c1ef069c2424440144e4327b111e8b
fredrvaa <fredrvaa@stud.ntnu.no>
2021-04-06 13:36:29 +0200

    Add forms

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast, Toast } from "react-toastify";
 
 function ManageCoursePage(props) {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
   return (
     <>
-      <h2>Mange Course</h2>
-      <h3>{props.match.params.slug} </h3>
+      <h2>Manage Course</h2>
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
